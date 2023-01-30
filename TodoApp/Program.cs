06fd7defc16be18e4fa7122e07337d54c98bf53c
Emma Forslund <emfo2102@student.miun.se>
2023-01-30 12:55:20 +0100

    MVC, static files, routing

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -6,13 +6,24 @@ namespace TodoApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
+            //Aktiverar MVC
             builder.Services.AddControllersWithViews();
 
 
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //Aktiverar möjlighet att använda statiska filer
+            app.UseStaticFiles();
+
+            //Aktiverar routing
+            app.UseRouting();
+
+
+            //Skapar routing
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
 
             app.Run();
         }
