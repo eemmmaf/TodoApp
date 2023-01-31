769b9cdb2f6983441e509ca87e21efc587bbd9a3
Emma Forslund <emfo2102@student.miun.se>
2023-01-31 15:10:58 +0100

    Skapat controller, views och layouy

---
+++ TodoApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Ändrat sökvägen till "Om-appen"-sidan
+        [Route("/omappen")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("/attgöra")]
+        public IActionResult TodoList()
+        {
+            //Läser in json och konverterar till IEnumerable
+            var jsonStr = System.IO.File.ReadAllText("Todos.json");
+            var jsonObj = JsonConvert.DeserializeObject<IEnumerable<Todos>>(jsonStr);
+
+            return View();
+        }
+    }
+}
---
+++ TodoApp/Models/Todos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace TodoApp.Models
+{
+    public class Todos
+    {
+
+        //-------------- Properties -------------//
+
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get;}
+        public DateTime? Date { get; set; }
+
+        public string? Priority { get; set; }
+        
+
+
+
+    }
+}
